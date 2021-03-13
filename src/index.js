8cb55303cab47e7acd3af60350d550bd4ccea3a9
Oleg Borovikov <hhyahhh@gmail.com>
2021-03-13 22:46:28 +0300

    Implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let minNum = array[0];
+    array.forEach(function(element) {
+        if (minNum > element)
+            minNum = element;
+    });
+    return minNum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let minNum = array[0];
+    array.forEach(function(element) {
+        if (minNum < element)
+            minNum = element;
+    });
+    return minNum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let sum = 0;
+    array.forEach(function(element) {
+        sum = sum + element;
+    });
+    return sum / array.length;
 }
